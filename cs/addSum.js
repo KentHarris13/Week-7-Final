d3290191cd177a1ca624a7f66f88335beac9a235
Kent Harris <KentHarris13@users.noreply.github.com>
2021-10-29 15:35:43 -0600

    correct 1

--- cs/addSum.js
+++ cs/addSum.js
@@ -1,14 +1,29 @@
-const array = [1, -1];
-let sum = 0;
+// const array = [1, -1, 0];
+// let sum = 0;
 
-for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-}
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+// }
 
-if (sum == 0){
-    console.log("True")
-}else {
-    console.log("False")
-}
-console.log(sum);
+// if (sum == 0){
+//     console.log("True")
+// }else {
+//     console.log("False")
+// }
+// console.log(sum);
+
+// Below is the correct code that you wanted. The code above is the one I thought was what you guys wanted.
+
+
+function toZero(arr){
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            sum = arr[i]+arr[j]
+        } 
+        if(sum === 0){
+            return true;}
+        }
+            return false;
+        }
 
